ffd83185943bfaa386b32621c9e5625755b176c3
cbxcvl <carloscavallinburatto@gmail.com>
2024-01-01 23:51:31 -0300

    feat: add ThrottlerModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,36 @@ import { UserModule } from './user/user.module';
 import { CustomerModule } from './customer/customer.module';
 import { BillingsModule } from './billings/billings.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, CustomerModule, BillingsModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    AuthModule,
+    UserModule,
+    CustomerModule,
+    BillingsModule,
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 2000,
+        limit: 2,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
+  ],
   controllers: [],
-  providers: [],
+  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
 })
 export class AppModule {}
