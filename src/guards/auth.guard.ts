25b7229b2a0af780abcc192c5d7fc89488b03dc0
cbxcvl <carloscavallinburatto@gmail.com>
2024-01-02 01:21:05 -0300

    feat: add auth

--- src/guards/auth.guard.ts
+++ src/guards/auth.guard.ts
@@ -1,11 +1,37 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  constructor(private readonly jwtService: JwtService) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    try {
+      const request = context.switchToHttp().getRequest();
+
+      const token = request.headers['authorization'];
+
+      if (!token || !token.startsWith('Bearer ')) {
+        throw new Error('Token de autorização ausente ou inválido');
+      }
+
+      const authToken = token.split(' ')[1];
+
+      const decodedToken = this.jwtService.verify(authToken);
+
+      if (!decodedToken) {
+        throw new Error('Falha na verificação do token JWT');
+      }
+
+      request.user = decodedToken;
+
+      return true;
+    } catch (error) {
+      console.error('Erro durante a autenticação:', error);
+      return false;
+    }
   }
 }
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,11 +3,19 @@ import { UserController } from './user.controller';
 import { PrismaService } from '../database/prisma.service';
 import { UserRepository } from '../repositories/user-repository';
 import { PrismaUserRepository } from '../repositories/prisma/prisma-user-repository';
-
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from '../auth/auth.service';
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_KEY,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [UserController],
   providers: [
     PrismaService,
+    AuthService,
     { provide: UserRepository, useClass: PrismaUserRepository },
   ],
 })
