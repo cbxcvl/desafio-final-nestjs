da66fc5a2105adc0d3019c8574d19397b1b02d1b
cbxcvl <carloscavallinburatto@gmail.com>
2024-01-05 18:37:22 -0300

    chore: customer module

--- src/customer/customer.module.ts
+++ src/customer/customer.module.ts
@@ -1,9 +1,19 @@
+// customer.module.ts
 import { Module } from '@nestjs/common';
-import { CustomerService } from './customer.service';
 import { CustomerController } from './customer.controller';
+import { CustomerRepository } from '../repositories/customer-repository';
+import { JwtModule } from '@nestjs/jwt';
+import { PrismaCustomerRepository } from '../repositories/prisma/prisma-customer-repository';
+import { AuthService } from '../auth/auth.service';
+import { PrismaService } from '../database/prisma.service';
 
 @Module({
+  imports: [JwtModule],
   controllers: [CustomerController],
-  providers: [CustomerService],
+  providers: [
+    PrismaService,
+    AuthService,
+    { provide: CustomerRepository, useClass: PrismaCustomerRepository },
+  ],
 })
 export class CustomerModule {}
