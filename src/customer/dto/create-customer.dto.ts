17f149fbd107cb00e2da47bbae01fb0f88f430e0
cbxcvl <carloscavallinburatto@gmail.com>
2024-01-05 18:39:07 -0300

    feat: add createCustomerDTO

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,1 +1,19 @@
-export class CreateCustomerDto {}
+import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+
+export class CreateCustomerDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  phone: string;
+
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  cpf: string;
+}
