4414b7cb2e0b1d0456705dc1e39b2bbed426036c
cbxcvl <carloscavallinburatto@gmail.com>
2024-01-11 02:30:01 -0300

    feat: add import to Module

--- src/billings/billings.module.ts
+++ src/billings/billings.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
-import { BillingsService } from './billings.service';
 import { BillingsController } from './billings.controller';
+import { PrismaService } from '../database/prisma.service';
+import { AuthService } from '../auth/auth.service';
+import { BillingRepository } from '../repositories/billing-repository';
+import { PrismaBillingRepository } from '../repositories/prisma/prisma-billing-repository';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_KEY,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
   controllers: [BillingsController],
-  providers: [BillingsService],
+  providers: [
+    PrismaService,
+    AuthService,
+    { provide: BillingRepository, useClass: PrismaBillingRepository },
+  ],
 })
 export class BillingsModule {}
