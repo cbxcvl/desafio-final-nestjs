1ddb462c571c57e8ebc8d88f705da2390b036e3d
cbxcvl <carloscavallinburatto@gmail.com>
2024-01-11 02:30:28 -0300

    feat: add createBillingDto

--- src/billings/dto/create-billing.dto.ts
+++ src/billings/dto/create-billing.dto.ts
@@ -1,1 +1,23 @@
-export class CreateBillingDto {}
+import { IsString, IsNotEmpty, IsInt, IsDateString } from 'class-validator';
+
+export class CreateBillingDto {
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  status: string;
+
+  @IsInt()
+  @IsNotEmpty()
+  value: number;
+
+  @IsDateString()
+  @IsNotEmpty()
+  dueDate: string;
+
+  @IsString()
+  @IsNotEmpty()
+  customerId: string;
+}
